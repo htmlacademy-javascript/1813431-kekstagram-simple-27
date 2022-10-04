01da071c628f67c15c5489e22e15a78681068de7
Vladimir Verbitskiy <vova10barcelona@mail.ru>
2022-10-05 02:15:31 +0300

    Добавляет функцию для проверки максимальной длины строки

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+const getMaxLength = function (string, maxString) {
+  if (string <= maxString) {
+    console.log('Строка проходит по длине');
+  } else {
+    console.log('Строка не проходит по длине');
+  }
+};
+
+getMaxLength();
