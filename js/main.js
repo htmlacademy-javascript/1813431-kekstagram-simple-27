7cbe616ba6d5f6f5f549520d14785144f9a4bb90
Vladimir Verbitskiy <vova10barcelona@mail.ru>
2022-10-05 23:19:41 +0300

    Добавляет функцию, возвращающую случайное целое число из диапазона

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
+const randomNumber = function (minNumber, maxNumber) {
+  const number = minNumber + Math.random() * (maxNumber - minNumber);
+  return Math.round(number);
+};
+
+randomNumber(1, 10);
+
+// Функция для проверки максимальной длины строки.
+
+let message;
 
 const getMaxLength = function (string, maxString) {
   if (string <= maxString) {
-    console.log('Строка проходит по длине');
+    message = 'Строка проходит по длине';
+    return message;
   } else {
-    console.log('Строка не проходит по длине');
+    message = 'Строка не проходит по длине';
+    return message;
   }
 };
 
-getMaxLength();
+getMaxLength(1, 5);
+
+
